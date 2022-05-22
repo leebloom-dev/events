18cff1c2d626ca9531a1a58c017e59cb0f241cfc
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-05-22 17:21:12 -0400

    added view form and form controller methods

--- src/main/java/com/events/events/controllers/EventController.java
+++ src/main/java/com/events/events/controllers/EventController.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("events") // URL: localhost:8080/events
@@ -15,8 +18,22 @@ public class EventController {
 
     // responds to GET requests at URL "/events"
     @GetMapping
-    public String displayAllEvents() {
+    public String displayAllEvents(Model model) {
+        model.addAttribute("events", events);
         return "/events/index";
+    }
+
+    // responds to GET requests at URL "/events/form"
+    @GetMapping("form")
+    public String displayEventForm() {
+        return "/events/form";
+    }
+
+    // responds to POST requests at URL "/events/form"
+    @PostMapping("form")
+    public String processEventForm(@RequestParam String name) {
+        events.add(name);
+        return "redirect:"; // to controller method with URL: "/events"
     }
 
 }
